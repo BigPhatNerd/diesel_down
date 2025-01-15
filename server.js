c740d42344109f1e4ad7b0710a86f466a236269f
BigPhatNerd <wilsonhorrell@gmail.com>
2025-01-15 10:44:39 -0600

    fdl

--- server.js
+++ server.js
@@ -71,7 +71,13 @@ app.get('/privacy-policy', (req, res) => {
 
 console.log({ dirname: __dirname })
 if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static('client/build'));
+
+  app.get('/blog/:slug', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
