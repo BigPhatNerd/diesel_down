5ef2412972cc4491999a4fbb14e6b41b6aea4f3c
BigPhatNerd <wilsonhorrell@gmail.com>
2025-01-15 11:40:01 -0600

    ddfkjls

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const app = express();
 connectDB();
 
 
-
-
-
 app.use((req, res, next) => {
   // Skip redirection for local development
   if (process.env.NODE_ENV !== 'production') {
@@ -63,30 +60,7 @@ app.get('/privacy-policy', (req, res) => {
 
 console.log({ dirname: __dirname })
 if (process.env.NODE_ENV === 'production') {
-
   app.use(express.static('client/build'));
-
-  app.get('/blog/:slug', async (req, res) => {
-    const slug = req.params.slug;
-
-    // Fetch the blog post from your API or database
-    const blog = await fetch(`https://api.dieseldown.com/blog/${slug}`).then((response) =>
-      response.json()
-    );
-
-    // Read the index.html file
-    const indexHTML = fs.readFileSync(path.resolve(__dirname, 'client', 'build', 'index.html'), 'utf8');
-
-    // Replace placeholders with dynamic meta tags
-    const updatedHTML = indexHTML
-      .replace('<meta property="og:title" content="Diesel Down - Performance Diesel Data and Analytics" />', `<meta property="og:title" content="${blog.title}" />`)
-      .replace('<meta property="og:description" content="Explore Diesel Down\'s professional diesel tuning services with our state-of-the-art Dynocom 15,000 Series Dyno!" />', `<meta property="og:description" content="${blog.excerpt}" />`)
-      .replace('<meta property="og:image" content="https://dieseldown.com/profile_avatar.jpg" />', `<meta property="og:image" content="${blog.image}" />`)
-      .replace('<meta property="og:url" content="https://dieseldown.com" />', `<meta property="og:url" content="https://dieseldown.com/blog/${slug}" />`);
-    console.log({ updatedHTML })
-    res.send(updatedHTML);
-  });
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
