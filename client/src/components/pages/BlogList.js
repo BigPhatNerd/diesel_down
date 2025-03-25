3348ab49a82850267be5ffbebac8927b8c4272a7
BigPhatNerd <wilsonhorrell@gmail.com>
2025-03-25 11:48:08 -0500

    dlkfas

--- client/src/components/pages/BlogList.js
+++ client/src/components/pages/BlogList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Container, Row, Card, Button } from "react-bootstrap";
+import { Container, Row, Card, Button, Pagination } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import NavigationLinks from "../NavigationLinks";
 import SocialMediaLinks from "./SocialMediaLinks";
@@ -14,6 +14,10 @@ const BlogList = () => {
     const [blogs, setBlogs] = useState([]);
     const [loading, setLoading] = useState(true);
     const [expanded, setExpanded] = useState(null);
+    console.log({ blogs })
+    // Pagination state
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 5; // Number of blogs per page
 
     const styles = getBackgroundStyles();
     const toggleExpand = (index) => {
@@ -40,6 +44,13 @@ const BlogList = () => {
         fetchBlogs();
     }, [setAlert]);
 
+    // Pagination logic
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentBlogs = blogs.slice(indexOfFirstPost, indexOfLastPost);
+
+    const handlePageChange = (pageNumber) => setCurrentPage(pageNumber);
+
     if (loading) return <p>Loading...</p>;
 
     return (
@@ -49,7 +60,7 @@ const BlogList = () => {
                     <h1>Blog Posts</h1>
                 </Row>
 
-                {blogs.map((blog, index) => (
+                {currentBlogs.map((blog, index) => (
                     <Card key={index} className="mb-3">
                         <Card.Header>
                             <Row
@@ -77,6 +88,20 @@ const BlogList = () => {
                         )}
                     </Card>
                 ))}
+
+
+                {/* Pagination Controls */}
+                <Pagination className="justify-content-center custom-pagination">
+                    {[...Array(Math.ceil(blogs.length / postsPerPage))].map((_, index) => (
+                        <Pagination.Item
+                            key={index + 1}
+                            active={index + 1 === currentPage}
+                            onClick={() => handlePageChange(index + 1)}
+                        >
+                            {index + 1}
+                        </Pagination.Item>
+                    ))}
+                </Pagination>
 
                 <AddressComponent />
 
